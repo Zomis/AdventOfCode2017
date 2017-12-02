1902c9aef59f0fb40c0e3b90d2745ed57ffbbfd4
Simon Forsberg <zomis2k@hotmail.com>
2017-12-02 13:15:55 +0100

    Put solutions inside classes to avoid naming collisions

--- src/main/java/Day1.kt
+++ src/main/java/Day1.kt
@@ -1,27 +1,30 @@
 import org.junit.Assert
 
-fun count(numbers: IntArray): Int {
-    return numbers.foldIndexed(0, { index, current: Int, value ->
-        val other: Int = numbers[(index + 1) % numbers.size]
-        val add: Int = if (other == value) value else 0
-        current + add
-    })
-}
+class Day1 {
 
-fun count(value: String): Int {
-    return count(value.toCharArray().map { Character.digit(it, 10) }.toIntArray())
-}
+    fun count(numbers: IntArray): Int {
+        return numbers.foldIndexed(0, { index, current: Int, value ->
+            val other: Int = numbers[(index + 1) % numbers.size]
+            val add: Int = if (other == value) value else 0
+            current + add
+        })
+    }
 
-class Day1 {}
+    fun count(value: String): Int {
+        return count(value.toCharArray().map { Character.digit(it, 10) }.toIntArray())
+    }
+
+}
 
 fun main(args: Array<String>) {
+    val d = Day1()
 
-    val text = Day1::class.java.classLoader.getResource("day1").readText().split('\n')
+    val text = d.javaClass.classLoader.getResource("day1").readText().split('\n')
     text.filter { it.contains('=') }.forEach {
         val before = it.substring(0, it.indexOf('='))
         val after = it.substring(it.indexOf('=') + 1).trim()
-        Assert.assertEquals("Mismatch for input " + before, after.toInt(), count(before))
+        Assert.assertEquals("Mismatch for input " + before, after.toInt(), d.count(before))
     }
-    println(count(text[text.size - 1]))
+    println(d.count(text[text.size - 1]))
 
 }
--- src/main/java/Day2.kt
+++ src/main/java/Day2.kt
@@ -1,26 +1,30 @@
 import org.junit.Assert
 import java.util.regex.Pattern
 
-fun highLowDiff(numbers: IntArray): Int {
-    return numbers.max()!! - numbers.min()!!
-}
+class Day2 {
 
-fun process(values: Array<IntArray>): Int {
-    return values.fold(0, { sum, next ->  sum + highLowDiff(next) })
+    fun highLowDiff(numbers: IntArray): Int {
+        return numbers.max()!! - numbers.min()!!
+    }
+
+    fun process(values: Array<IntArray>): Int {
+        return values.fold(0, { sum, next ->  sum + highLowDiff(next) })
+    }
+
+    fun readText(data: String): Array<IntArray> {
+        return data.split('\n').map { s -> s.trim().split(Pattern.compile("\\s+"))
+                .map { it.trim().toInt() }.toIntArray() }.toTypedArray()
+    }
 }
 
 fun main(args: Array<String>) {
     val test = arrayOf(
-        intArrayOf(5, 1, 9, 5),
-        intArrayOf(7, 5, 3),
-        intArrayOf(2, 4, 6, 8)
+            intArrayOf(5, 1, 9, 5),
+            intArrayOf(7, 5, 3),
+            intArrayOf(2, 4, 6, 8)
     )
-    Assert.assertEquals(18, process(test))
+    val d2 = Day2()
+    Assert.assertEquals(18, d2.process(test))
 
-    println(process(readText(Res::class.java.classLoader.getResource("day2").readText())))
-}
-
-fun readText(data: String): Array<IntArray> {
-    return data.split('\n').map { s -> s.trim().split(Pattern.compile("\\s+"))
-        .map { it.trim().toInt() }.toIntArray() }.toTypedArray()
+    println(d2.process(d2.readText(Res::class.java.classLoader.getResource("day2").readText())))
 }
