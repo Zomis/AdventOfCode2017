2fb78652936c953ca92e1792f558748dacffa76e
Simon Forsberg <zomis2k@hotmail.com>
2017-12-08 22:54:39 +0100

    Add day7 input with empty implementation (solved part 1 manually)

---
+++ src/main/java/Day7.kt
@@ -0,0 +1,14 @@
+class Day7: Day<List<String>> {
+    override fun parse(text: String): List<String> {
+        return emptyList()
+    }
+
+    override fun part1(input: List<String>): Any {
+        return 0
+    }
+
+    override fun part2(input: List<String>): Any {
+        return 0
+    }
+
+}
