04e416ea31083cbc9d6a03e86df1f42a501d7399
Simon Forsberg <zomis2k@hotmail.com>
2017-12-17 20:36:51 +0100

    Add Day 16 part one

---
+++ src/main/java/Day16.kt
@@ -0,0 +1,47 @@
+
+typealias DanceMove = (MutableList<String>) -> Unit
+class Day16: Day<List<DanceMove>> {
+    override fun parse(text: String): List<DanceMove> {
+        return text.split(",").map { createMove(it) }
+    }
+
+    private fun createMove(it: String): DanceMove {
+        val params = it.substring(1).split('/')
+        when (it[0]) {
+            'x' -> return {
+                val intParams = params.map { it.toInt() }
+                // Swap indices
+                val tmp = it[intParams[0]]
+                it[intParams[0]] = it[intParams[1]]
+                it[intParams[1]] = tmp
+            }
+            's' -> return {
+                val count = params[0].toInt()
+                // Spin
+                for (i in 1..count) {
+                    val value = it.removeAt(it.size - 1)
+                    it.add(0, value)
+                }
+            }
+            'p' -> return {
+                // Swap program names
+                val index0 = it.indexOf(params[0])
+                val index1 = it.indexOf(params[1])
+                createMove("x$index0/$index1").invoke(it)
+            }
+            else -> throw IllegalArgumentException(it)
+        }
+    }
+
+    override fun part1(input: List<DanceMove>): Any {
+        val range = 'a'..'p'
+        val list = range.map { it.toString() }.toMutableList()
+        input.forEach { it.invoke(list) }
+        return list.joinToString("")
+    }
+
+    override fun part2(input: List<DanceMove>): Any {
+        return 0
+    }
+
+}
