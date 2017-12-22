421c9c9b55b3eb142347d71ed44dac4fa25bdd37
Simon Forsberg <zomis2k@hotmail.com>
2017-12-22 18:51:26 +0100

    Refactor Day 18

--- src/main/java/Day18.kt
+++ src/main/java/Day18.kt
@@ -1,3 +1,8 @@
+inline fun MutableMap<String, Long>.numberOrMapValue(value: String): Long {
+    val number = Regex("-?\\d+").matches(value)
+    return if (number) value.toLong() else getOrElse(value, { 0 })
+}
+
 class Day18: Day<List<Day18.Instruction18>> {
     val SOUND_VALUE = "SOUND_VALUE"
 
@@ -8,43 +13,52 @@ class Day18: Day<List<Day18.Instruction18>> {
     }
 
     override fun part1(input: List<Instruction18>): Any {
-        val values = mutableMapOf<String, Long>()
+        val vals = mutableMapOf<String, Long>()
+        val prog = Program(input, vals, { instruction, values -> values[SOUND_VALUE] = values.numberOrMapValue(instruction.first); 1},
+        { instruction, values -> if (values.getOrElse(instruction.first, {0}) != 0.toLong()) values[instruction.first] = values.getOrElse(SOUND_VALUE, {0}); 1})
+
+        while (prog.instruction in 0..input.size) {
+            val result = prog.runStep()
+            if (result.first.type == "rcv" && vals[SOUND_VALUE] ?: 0 != 0.toLong()) {
+                return vals[SOUND_VALUE] ?: 0
+            }
+        }
+
+        return vals[SOUND_VALUE] ?: 0
+    }
+
+    class Program(val instructions: List<Instruction18>,
+            private val values: MutableMap<String, Long>, val sndAction: (Instruction18, MutableMap<String, Long>) -> Int,
+              val rcvAction: (Instruction18, MutableMap<String, Long>) -> Int) {
         var instruction = 0
-        while (instruction in 0..input.size) {
-            val ins = input[instruction]
+
+        fun runStep(): Pair<Instruction18, Int> {
+            val ins = instructions[instruction]
             val result = perform(values, ins)
             instruction += result
-            if (ins.type == "rcv" && values[SOUND_VALUE] ?: 0 != 0.toLong()) {
-                return values[SOUND_VALUE] ?: 0
-            }
+            return Pair(ins, result)
         }
-        return values[SOUND_VALUE] ?: 0
-    }
 
-    private fun perform(values: MutableMap<String, Long>, instruction: Instruction18): Int {
-        println("Perform $instruction at $values")
-        // Thread.sleep(100)
-        when (instruction.type) {
-            "set" -> values[instruction.first] = mapValue(values, instruction.second)
-            "add" -> values[instruction.first] = values.getOrElse(instruction.first, {0}) + mapValue(values, instruction.second)
-            "mul" -> values[instruction.first] = values.getOrElse(instruction.first, {0}) * mapValue(values, instruction.second)
-            "mod" -> values[instruction.first] = values.getOrElse(instruction.first, {0}) % mapValue(values, instruction.second)
-            "rcv" -> if (values.getOrElse(instruction.first, {0}) != 0.toLong()) values[instruction.first] = values.getOrElse(SOUND_VALUE, {0})
-            "snd" -> values[SOUND_VALUE] = mapValue(values, instruction.first)
-            "jgz" -> {
-                if (values[instruction.first] ?: 0 > 0) {
-                    val value: Long = mapValue(values, instruction.second)
-                    println("Jump $value")
-                    return value.toInt()
+        private fun perform(values: MutableMap<String, Long>, instruction: Instruction18): Int {
+            println("Perform $instruction at $values")
+            // Thread.sleep(100)
+            when (instruction.type) {
+                "set" -> values[instruction.first] = values.numberOrMapValue(instruction.second)
+                "add" -> values[instruction.first] = values.getOrElse(instruction.first, {0}) + values.numberOrMapValue(instruction.second)
+                "mul" -> values[instruction.first] = values.getOrElse(instruction.first, {0}) * values.numberOrMapValue(instruction.second)
+                "mod" -> values[instruction.first] = values.getOrElse(instruction.first, {0}) % values.numberOrMapValue(instruction.second)
+                "rcv" -> return rcvAction(instruction, values)
+                "snd" -> return sndAction(instruction, values)
+                "jgz" -> {
+                    if (values[instruction.first] ?: 0 > 0) {
+                        val value: Long = values.numberOrMapValue(instruction.second)
+                        println("Jump $value")
+                        return value.toInt()
+                    }
                 }
             }
+            return 1
         }
-        return 1
-    }
-
-    private fun mapValue(values: MutableMap<String, Long>, value: String): Long {
-        val number = Regex("-?\\d+").matches(value)
-        return if (number) value.toLong() else values.getOrElse(value, { 0 })
     }
 
     override fun part2(input: List<Instruction18>): Any {
