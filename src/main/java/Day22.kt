9b0ea5d751e09909a1317538ddf4857e97b310e5
Simon Forsberg <zomis2k@hotmail.com>
2017-12-23 14:25:54 +0100

    Day 22: Extract functions

--- src/main/java/Day22.kt
+++ src/main/java/Day22.kt
@@ -7,6 +7,42 @@ class Day22: Day<Array<BooleanArray>> {
         CLEAN, WEAKENED, INFECTED, FLAGGED
     }
 
+    fun rotate1(state: NodeState): (Dir4) -> Dir4 {
+        return when (state) {
+            NodeState.INFECTED -> Dir4::rotateRight
+            NodeState.CLEAN -> Dir4::rotateLeft
+            else -> throw IllegalStateException(state.toString())
+        }
+    }
+
+    fun stateChange1(state: NodeState): NodeState {
+        return when (state) {
+            NodeState.CLEAN -> NodeState.INFECTED
+            NodeState.INFECTED -> NodeState.CLEAN
+            else -> throw IllegalStateException(state.toString())
+        }
+    }
+
+    fun run(map: MutableMap<Pair<Int, Int>, NodeState>, rotate: (NodeState) -> (Dir4) -> Dir4, stateChange: (NodeState) -> NodeState): Int {
+        var virusX = 0
+        var virusY = 0
+        var dir = Dir4.UP
+        var count = 0
+        for (i in 0 until 10000) {
+            val pair = Pair(virusX, virusY)
+            val oldState = map[pair] ?: NodeState.CLEAN
+            dir = rotate(oldState)(dir)
+            val nextState = stateChange(oldState)
+            map[pair] = nextState
+            if (nextState == NodeState.INFECTED) {
+                count++
+            }
+            virusX += dir.x
+            virusY += dir.y
+        }
+        return count
+    }
+
     override fun part1(input: Array<BooleanArray>): Any {
         val map = mutableMapOf<Pair<Int, Int>, NodeState>()
         val origoY = input.size / 2
@@ -17,26 +53,7 @@ class Day22: Day<Array<BooleanArray>> {
             })
         })
 
-        var virusX = 0
-        var virusY = 0
-        var dir = Dir4.UP
-        var count = 0
-        for (i in 0 until 10000) {
-            val pair = Pair(virusX, virusY)
-            if (map[pair] == NodeState.INFECTED) {
-                dir = dir.rotateRight()
-                map[pair] = NodeState.CLEAN
-            } else {
-                dir = dir.rotateLeft()
-                count++
-                map[pair] = NodeState.INFECTED
-            }
-            virusX += dir.x
-            virusY += dir.y
-            // map[pair] = !map.getOrElse(pair, {false})
-        }
-        // 5520 is too low
-        return count
+        return run(map, ::rotate1, ::stateChange1)
     }
 
     override fun part2(input: Array<BooleanArray>): Any {
