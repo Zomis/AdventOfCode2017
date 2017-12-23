18cb4822362b5ab9a6ce42708076be4b0f00f8a8
Simon Forsberg <zomis2k@hotmail.com>
2017-12-23 19:40:35 +0100

    Solve Day 20 part two in a not so nice way

--- src/main/java/Day20.kt
+++ src/main/java/Day20.kt
@@ -20,18 +20,41 @@ class Day20: Day<List<Day20.PosVecAcc>> {
     }
 
     override fun part2(input: List<PosVecAcc>): Any {
-        return 0
+        var remaining = input.toMutableList()
+        for (i in 1..100_000) {
+            //println("Remaining: ${remaining.size}")
+            val positions = remaining.groupBy { it.pos }.filter { it.value.size > 1 }.flatMap { it.value }
+            remaining.removeAll(positions)
+            remaining = remaining.map { it.step() }.toMutableList()
+        }
+        return remaining.size
     }
 
     data class XYZ(val x: Int, val y: Int, val z: Int) {
         fun manhattanDistance(): Int {
             return Math.abs(x) + Math.abs(y) + Math.abs(z)
         }
+
         fun length(): Double {
             return Math.sqrt((x*x + y*y + z*z).toDouble())
         }
+
+        fun add(other: XYZ): XYZ {
+            return XYZ(x + other.x, y + other.y, z + other.z)
+        }
     }
-    data class PosVecAcc(val pos: XYZ, val velocity: XYZ, val acc: XYZ)
+
+    data class PosVecAcc(val pos: XYZ, val velocity: XYZ, val acc: XYZ) {
+        fun step(): PosVecAcc {
+            val nextVelocity = velocity.add(acc)
+            val nextPosition = pos.add(nextVelocity)
+            return PosVecAcc(nextPosition, nextVelocity, acc)
+        }
+
+        fun collidesWith(other: PosVecAcc): Boolean {
+            return pos == other.pos
+        }
+    }
 
 
 
