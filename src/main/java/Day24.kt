ab17ff1b4714c96eb7d2b2edad9beca4d98fd09d
Simon Forsberg <zomis2k@hotmail.com>
2017-12-24 23:28:14 +0100

    Solve Day 24 part two

--- src/main/java/Day24.kt
+++ src/main/java/Day24.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 typealias BridgePiece = Pair<Int, Int>
 
 class Day24: Day<List<BridgePiece>> {
@@ -11,13 +13,14 @@ class Day24: Day<List<BridgePiece>> {
     }
 
     override fun part1(input: List<BridgePiece>): Any {
-        return solve(emptyList(), 0, 0, input)
+        return solve(0, 0, 0, input, Comparator({ a, b -> a.strength - b.strength })).strength
     }
 
-    private fun solve(used: List<BridgePiece>, current: Int, sum: Int, remaining: List<BridgePiece>): Int {
+    data class BridgeResult(val length: Int, val strength: Int)
+    private fun solve(current: Int, length: Int, strength: Int, remaining: List<BridgePiece>, comparator: Comparator<BridgeResult>): BridgeResult {
         val options = remaining.filter { it.first == current || it.second == current }
         if (options.isEmpty()) {
-            return sum
+            return BridgeResult(length, strength)
         }
 //        println("Used $used has options $options current $current sum $sum")
 
@@ -25,14 +28,17 @@ class Day24: Day<List<BridgePiece>> {
             val next = if (it.first == current) it.second else it.first
             val nextList = remaining.toMutableList()
             nextList.remove(it)
-            val usedNext = used.toMutableList()
-            usedNext.add(it)
-            solve(usedNext, next, sum + it.first + it.second, nextList)
-        }.max()!!
+//            val usedNext = used.toMutableList()
+//            usedNext.add(it)
+            solve(next, length + 1,strength + it.first + it.second, nextList, comparator)
+        }.maxWith(comparator)!!
     }
 
     override fun part2(input: List<BridgePiece>): Any {
-        return 0
+        return solve(0, 0, 0, input, Comparator({ a, b ->
+            val lengthDiff = a.length - b.length
+            if (lengthDiff != 0) lengthDiff else a.strength - b.strength
+        })).strength
     }
 
 }
