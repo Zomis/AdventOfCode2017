7920a9115cc90b40d7a310ca73436d6c0b28bb8b
Simon Forsberg <zomis2k@hotmail.com>
2017-12-25 15:51:52 +0100

    Extract IntProgression in Main

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,13 +1,14 @@
 fun main(args: Array<String>) {
-
-    for (day in 1..25) {
+    var range: IntProgression = 1..25
+    //range = 25 downTo 1
+    for (day in range) {
         val d: Day<Any>
         try {
             val cl = Day::class.java.classLoader.loadClass("Day$day")
             println(cl.simpleName)
             d = cl.newInstance() as Day<Any>
         } catch (ex: ClassNotFoundException) {
-            return
+            continue
         }
         println("Load $day")
         val url = d.javaClass.classLoader.getResource("day$day")
