e72abb43db85fbb99b99bb32e0cea81164af7ed2
Simon Forsberg <zomis2k@hotmail.com>
2019-01-04 14:39:12 +0100

    Improve Day 4

--- 2018-python3/day4.py
+++ 2018-python3/day4.py
@@ -2,8 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from days import read_file
 from enum import Enum
-from collections import defaultdict
-from itertools import groupby
+from collections import defaultdict, namedtuple
 from statistics import mode
 import statistics
 import operator
@@ -36,8 +35,8 @@ class GuardSleep:
 
 def get_guard(line: str):
     if "Guard" in line:
-        id = re.search("Guard #(\d+)", line)
-        return int(id.group(1))
+        guard_id = re.search("Guard #(\\d+)", line)
+        return int(guard_id.group(1))
     return -1
 
 
@@ -51,49 +50,42 @@ def event_type(line):
     raise Exception("Unknown line: " + line)
 
 
-def part1() -> int:
+def day4() -> (int, int):
     events = sorted(list(Event(datetime.strptime(line[1:17], "%Y-%m-%d %H:%M"), get_guard(line), event_type(line)) for line in read_file(4)), key=operator.attrgetter("time"))
 
     guard = -1
-    sleeping = False
     guardsleep = defaultdict(lambda: GuardSleep(0, 0, []))
     for event in events:
-        print(event)
         if event.guard >= 0:
             guard = event.guard
-            sleeping = False
 
         if event.event == EventType.FALLS_ASLEEP:
             guardsleep[guard].last_sleep = event.time.minute
-            sleeping = True
 
         if event.event == EventType.WAKES_UP:
             guardsleep[guard].sleep_total += event.time.minute - guardsleep[guard].last_sleep
             guardsleep[guard].add_sleeps(event.time.minute)
-            sleeping = False
 
-        # if sleeping:
-        #     guardsleep[guard].sleeps.append(event.time.minute)
-
-    print(f"Sleepy guard is {guardsleep}")
     most_sleepy_guard_number = max(guardsleep, key=(lambda key: guardsleep[key].sleep_total))
-    print(f"Most sleepy guards is {most_sleepy_guard_number}")
 
     most_sleepy_guard = guardsleep[most_sleepy_guard_number]
-    print(sorted(most_sleepy_guard.sleeps))
-    print(mode(sorted(most_sleepy_guard.sleeps)))
+    part1_result = most_sleepy_guard_number * mode(sorted(most_sleepy_guard.sleeps))
 
+    # Part 2
+    MostSleepy = namedtuple('MostCommon', ['id', 'minute', 'amount'])
+    most_sleepy = MostSleepy(0, 0, 0)
     for k in guardsleep:
         current_guard = guardsleep[k]
         try:
-            most_common = mode(sorted(current_guard.sleeps))
-            amount = len(list((m for m in current_guard.sleeps if m == most_common)))
-            print(f"{k} has {most_common} with {amount}")
+            most_common_minute = mode(sorted(current_guard.sleeps))
+            amount = len(list((m for m in current_guard.sleeps if m == most_common_minute)))
+            if amount > most_sleepy.amount:
+                most_sleepy = MostSleepy(k, most_common_minute, amount)
         except statistics.StatisticsError:
-            print("No special for " + str(k))
+            print("No unique most common minute for " + str(k))
+
+    return part1_result, most_sleepy.id * most_sleepy.minute
 
 
-    return 42
-
-
-part1()
+if __name__ == '__main__':
+    print(day4())
