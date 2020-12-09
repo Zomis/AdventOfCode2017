0559256734d59946378f7930375a03a3afc81bc1
Simon Forsberg <zomis2k@hotmail.com>
2020-12-09 13:56:10 +0100

    Refactor 2020 Day 1

--- 2020-kotlin/src/main/kotlin/Day1.kt
+++ 2020-kotlin/src/main/kotlin/Day1.kt
@@ -1,26 +1,22 @@
 package net.zomis.aoc.y2020
 
-class Day1 {
+object Day1 {
 
-    fun sfd(input: Sequence<String>) {
-        val numbers = input.map { it.toInt() }.toSet()
-        val match = numbers.filter { numbers.contains(2020 - it) }
-        println(match)
-        println(match.reduce { acc, i -> acc * i })
-
-        val m2 = numbers.map {
-            findSum(numbers.minus(it), 2020 - it)
-        }.filter { it.isNotEmpty() }.flatMap { it }.distinct()
-        println(m2)
-        println(m2.reduce { acc, i -> acc * i })
+    val day = Days.day<Set<Long>>(this) {
+        input {
+            map { it.toLong() }.toSet()
+        }
+        part1 {
+            findSum(input, 2020).reduce { acc, i -> acc * i }
+        }
+        part2 {
+            input.asSequence().map {
+                findSum(input.minus(it), 2020 - it)
+            }.filter { it.isNotEmpty() }.flatten().distinct().reduce { acc, i -> acc * i }
+        }
     }
 
-    fun findSum(numbers: Iterable<Int>, target: Int): List<Int> =
+    fun findSum(numbers: Iterable<Long>, target: Long): List<Long> =
         numbers.filter { numbers.contains(target - it) }
 
-}
-
-fun main() {
-    val input = Day1::class.java.classLoader.getResourceAsStream("day1.txt").bufferedReader().lineSequence()
-    Day1().sfd(input)
 }
